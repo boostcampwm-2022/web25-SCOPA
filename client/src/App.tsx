c9fb17477886ec2eca9c1ed4f99390e6c02ab42f
chichoon <chichoon.choi@gmail.com>
2022-11-14 21:20:34 +0900

    feat: 로그인 라우터 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
+import { LoginPage } from 'pages';
 
 // 라우팅은 이곳에
-function App() {
+const App = () => {
   return (
     <Routes>
       <Route path='/' element={<div>Hello world!!</div>} />
+      <Route path='/login' element={<LoginPage />} />
     </Routes>
   );
-}
+};
 
 export default App;
