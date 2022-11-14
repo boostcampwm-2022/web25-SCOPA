d22713f11ed80310fd51f92ef40d66faec2ce5bc
chichoon <chichoon.choi@gmail.com>
2022-11-14 22:45:26 +0900

    design: 제목, 소제목 추가

--- client/src/pages/LoginPage/index.tsx
+++ client/src/pages/LoginPage/index.tsx
@@ -1,12 +1,21 @@
 /** @jsxImportSource @emotion/react */
 
-import { loginPageBackgroundStyle, loginPageInnerStyle, loginPageWrapperStyle } from './styles';
+import {
+  loginPageBackgroundStyle,
+  loginPageHeaderStyle,
+  loginPageInnerStyle,
+  loginPageSubHeaderStyle,
+  loginPageWrapperStyle,
+} from './styles';
 
 export const LoginPage = () => {
   return (
     <div css={loginPageBackgroundStyle}>
       <div css={loginPageWrapperStyle}>
-        <div css={loginPageInnerStyle}>로그인 페이지입니다</div>
+        <div css={loginPageInnerStyle}>
+          <h1 css={loginPageHeaderStyle}>SCOPA</h1>
+          <h3 css={loginPageSubHeaderStyle}>나와 찰떡궁합인 팀원 찾기</h3>
+        </div>
       </div>
     </div>
   );
--- client/src/pages/LoginPage/styles.ts
+++ client/src/pages/LoginPage/styles.ts
@@ -1,6 +1,6 @@
 import { css } from '@emotion/react';
 import { COLORS } from 'styles/colors';
-import { COMMON_SIZE } from 'styles/sizes';
+import { COMMON_SIZE, FONT_SIZE } from 'styles/sizes';
 
 export const loginPageBackgroundStyle = css({
   backgroundImage: `url('./loginBG.jpg')`,
@@ -23,5 +23,17 @@ export const loginPageInnerStyle = css({
   width: COMMON_SIZE.LOGIN_BOX_WIDTH,
   height: COMMON_SIZE.LOGIN_BOX_HEIGHT,
   padding: COMMON_SIZE.LOGIN_BOX_PADDING,
+  borderRadius: COMMON_SIZE.BORDER_RADIUS,
   backgroundColor: COLORS.SECONDARY_1,
+});
+
+export const loginPageHeaderStyle = css({
+  fontSize: FONT_SIZE.TITLE_48,
+  color: COLORS.TEXT_1,
+  textAlign: 'center',
+});
+
+export const loginPageSubHeaderStyle = css({
+  fontSize: FONT_SIZE.LARGE,
+  textAlign: 'center',
 });
