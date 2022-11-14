e4bb4e0f39e6de1844fa64ec7038df71d66a5dec
dohyeon-han <limited.hyeon@gmail.com>
2022-11-14 15:47:03 +0900

    feat: Google OAuth로 유저정보 획득

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -6,21 +6,17 @@ import {
   Patch,
   Param,
   Delete,
+  Query,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { UpdateAuthDto } from './dto/update-auth.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.authService.remove(+id);
+  @Get('/google-callback')
+  async GoogleCallback(@Query('code') code: string) {
+    const userInfo = await this.authService.getGooleInfo(code);
+    return userInfo;
   }
 }
--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,26 +1,43 @@
 import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import axios from 'axios';
+
+// const GOOGLE_AUTH_URL = 'https://accounts.google.com/o/oauth2/v2/auth';
+const GOOGLE_AUTH_TOKEN_URL = 'https://oauth2.googleapis.com/token';
+const GOOGLE_AUTH_REDIRECT_URL = 'http://localhost:3001/auth/google-callback';
+
+export interface UserInfo {
+  email: string;
+  picture: string;
+}
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
-  }
+  async getGooleInfo(code: string): Promise<UserInfo> {
+    const { data } = await axios({
+      method: 'POST',
+      url: GOOGLE_AUTH_TOKEN_URL,
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded;charset=utf-8',
+      },
+      params: {
+        grant_type: 'authorization_code', //특정 스트링
+        client_id: process.env.GOOGLE_CLIENT_ID,
+        client_secret: process.env.GOOGLE_CLIENT_SECRET,
+        redirectUri: GOOGLE_AUTH_REDIRECT_URL,
+        code: code,
+      },
+    });
+    const accessToken = data['access_token'];
 
-  findAll() {
-    return `This action returns all auth`;
-  }
+    const { data: userData } = await axios.get(
+      `https://www.googleapis.com/oauth2/v3/userinfo?access_token=${accessToken}`,
+    );
+    const { email, picture } = userData;
+    const userInfo = {
+      email,
+      picture,
+    };
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+    return userInfo;
   }
 }
