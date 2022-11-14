d7dd18a0d6fdf16fe1db2683500ac8fd597ad597
chichoon <chichoon.choi@gmail.com>
2022-11-14 14:08:54 +0900

    feat: 프로젝트 initialize
    - BrowserRouter 적용
    - 루트 경로에 hello world 출력
    - reset.css 적용
    
    Co-authored-by: HSeo-git <sophieseo3@gmail.com>

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+import App from './App';
+
+import './reset.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    {/* RecoilRoot 등의 요소들 여기에 포함 */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
