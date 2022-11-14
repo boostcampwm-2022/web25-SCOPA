aacfc7e36d088d090481eedad0f4670bbc9557fd
chichoon <chichoon.choi@gmail.com>
2022-11-14 21:53:41 +0900

    design: 로그인 페이지 레이아웃 구성

--- client/src/pages/LoginPage/index.tsx
+++ client/src/pages/LoginPage/index.tsx
@@ -1,3 +1,13 @@
+/** @jsxImportSource @emotion/react */
+
+import { loginPageBackgroundStyle, loginPageInnerStyle, loginPageWrapperStyle } from './styles';
+
 export const LoginPage = () => {
-  return <div>로그인 페이지입니다</div>;
+  return (
+    <div css={loginPageBackgroundStyle}>
+      <div css={loginPageWrapperStyle}>
+        <div css={loginPageInnerStyle}>로그인 페이지입니다</div>
+      </div>
+    </div>
+  );
 };
