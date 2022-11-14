24f24d4acb7ef1141208b60b3075031053a866fa
chichoon <chichoon.choi@gmail.com>
2022-11-14 14:09:14 +0900

    design: 색상, 크기 관련 상수 분리
    Co-authored-by: HSeo-git <sophieseo3@gmail.com>

---
+++ client/src/styles/colors.ts
@@ -0,0 +1,8 @@
+export const COLORS = {
+  PRIMARY1: '#aac4ff',
+  PRIMARY2: '#b1b2ff',
+  SECONDARY1: '#d2daff',
+  SECONDARY2: '#eef1ff',
+  TEXT1: '#2c2c40',
+  TEXT2: '#585980',
+};
---
+++ client/src/styles/sizes.ts
@@ -0,0 +1,7 @@
+export const FONT_SIZE = {
+  TITLE1: 48,
+  TITLE2: 32,
+  TITLE3: 24,
+  NORMAL1: 16,
+  NORMAL2: 12,
+};
