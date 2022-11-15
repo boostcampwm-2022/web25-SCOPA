e586e28a77d7e101f4558195b01d08b83becf6c1
chichoon <chichoon.choi@gmail.com>
2022-11-15 20:05:36 +0900

    feat: main page 임시 구현

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
-import { LoginPage, TestPage } from 'pages';
+import { LoginPage, MainPage, TestPage } from 'pages';
 import { LoginLayout } from 'common';
 
 // 라우팅은 이곳에
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<div>Hello world!!</div>} />
+      <Route path='/' element={<MainPage />} />
       <Route path='/' element={<LoginLayout />}>
         <Route path='/login' element={<LoginPage />} />
         <Route path='/test' element={<TestPage />} />
--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -1,3 +1,5 @@
+export { MainPage } from './MainPage';
+
 export { LoginPage } from './LoginPage';
 export { RegisterPage } from './RegisterPage';
 
