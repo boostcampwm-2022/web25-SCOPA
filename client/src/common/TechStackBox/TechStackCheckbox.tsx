a3e05d38cc24f1a2de35db1c4001950cb68f7416
chichoon <chichoon.choi@gmail.com>
2022-11-15 15:08:47 +0900

    feat: 체크박스 컴포넌트 작성

---
+++ client/src/common/TechStackBox/TechStackCheckbox.tsx
@@ -0,0 +1,22 @@
+import { useCallback, Dispatch, SetStateAction, useState } from 'react';
+
+interface Props {
+  setSelectedStacks: Dispatch<SetStateAction<Array<string>>>;
+  name: string;
+}
+
+export const TechStackCheckbox = ({ setSelectedStacks, name }: Props) => {
+  const [isChecked, setIsChecked] = useState<boolean>(false);
+  const handleChangeCheckbox = useCallback(function () {
+    setIsChecked((prev) => !prev);
+    if (isChecked) setSelectedStacks((prev) => [...prev, name]);
+    else setSelectedStacks((prev) => prev.filter((value) => value !== name));
+  }, []);
+
+  return (
+    <>
+      <input type='checkbox' id={`tech-stack-${name}`} checked={isChecked} onChange={handleChangeCheckbox} />
+      <label htmlFor={`tech-stack-${name}`}>{name}</label>
+    </>
+  );
+};
