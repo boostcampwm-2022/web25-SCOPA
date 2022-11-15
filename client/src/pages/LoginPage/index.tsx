00dd14d42e765e9dace67b6d574bb6095b310ba7
chichoon <chichoon.choi@gmail.com>
2022-11-15 21:12:51 +0900

    feat: 로그인 페이지 렌더링 시에 로그인 여부 확인 후 메인 페이지로 리디렉션
    - recoil value 가져와서 사용
    - useEffect로 렌더링 될 때마다 체크

--- client/src/pages/LoginPage/index.tsx
+++ client/src/pages/LoginPage/index.tsx
@@ -1,6 +1,11 @@
 /** @jsxImportSource @emotion/react */
 
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+
 import { LoginButtonComponent } from './LoginButtonComponent';
+import { currentUserState } from 'store';
 
 import {
   loginPageHeaderStyle,
@@ -12,6 +17,13 @@ import {
 import { GithubIcon, GoogleIcon } from 'assets/svgs';
 
 export const LoginPage = () => {
+  const currentUser = useRecoilValue(currentUserState);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (currentUser.id !== null) navigate('/'); // 로그인이 되어 있을 경우, 메인 페이지로 리디렉션
+  }, [currentUser]);
+
   return (
     <>
       <div css={loginPageHeaderWrapperStyle}>
