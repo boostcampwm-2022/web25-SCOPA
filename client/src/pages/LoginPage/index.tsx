65b0e79d245f56c027a8b0e311c54ac590242a07
chichoon <chichoon.choi@gmail.com>
2022-11-15 12:57:50 +0900

    feat: 깃허브 로그인 링크, 구글 로그인 링크 걸기

--- client/src/pages/LoginPage/index.tsx
+++ client/src/pages/LoginPage/index.tsx
@@ -26,8 +26,17 @@ export const LoginPage = () => {
             <img src='/earlybird.png' alt='team earlybird' css={loginPageLogoImageStyle} />
           </div>
           <h3 css={loginPageSubHeaderStyle}>나와 찰떡궁합인 팀원 찾기</h3>
-          <LoginButtonComponent link='#' icon={<GithubIcon />} innerText='Github' isBackgroundBlack />
-          <LoginButtonComponent link='#' icon={<GoogleIcon />} innerText='Google' />
+          <LoginButtonComponent
+            link={process.env.REACT_APP_GITHUB_OAUTH as string}
+            icon={<GithubIcon />}
+            innerText='Github'
+            isBackgroundBlack
+          />
+          <LoginButtonComponent
+            link={process.env.REACT_APP_GOOGLE_OAUTH as string}
+            icon={<GoogleIcon />}
+            innerText='Google'
+          />
         </div>
       </div>
     </div>
