07cc45fbbbf318f419766b3c6dd44bc567e7d6de
chichoon <chichoon.choi@gmail.com>
2022-11-15 16:20:54 +0900

    feat: Tech Stack Box 구성
    - 디자인 포함

--- client/src/common/TechStackBox/index.tsx
+++ client/src/common/TechStackBox/index.tsx
@@ -1,3 +1,21 @@
-export const TechStackBox = () => {
-  return <div>기술 스택 박스</div>;
+/** @jsxImportSource @emotion/react */
+
+import { SetStateAction, Dispatch } from 'react';
+import { STACK_LIST } from 'utils/constants';
+import { TechStackCheckbox } from './TechStackCheckbox';
+
+import { techStackBoxWrapper } from './styles';
+
+interface Props {
+  setSelectedStacks: Dispatch<SetStateAction<Array<string>>>;
+}
+
+export const TechStackBox = ({ setSelectedStacks }: Props) => {
+  return (
+    <div css={techStackBoxWrapper}>
+      {STACK_LIST.map((stack) => (
+        <TechStackCheckbox key={`tech-stack-${stack}`} setSelectedStacks={setSelectedStacks} name={stack} />
+      ))}
+    </div>
+  );
 };
---
+++ client/src/common/TechStackBox/styles.ts
@@ -0,0 +1,16 @@
+import { css } from '@emotion/react';
+
+import { COLORS } from 'styles/colors';
+import { COMMON_SIZE } from 'styles/sizes';
+
+export const techStackBoxWrapper = css({
+  position: 'absolute',
+  display: 'grid',
+  width: 480,
+  height: 320,
+  gridTemplateColumns: 'repeat(3, 1fr)',
+  backgroundColor: COLORS.SECONDARY_2,
+  border: `${COMMON_SIZE.POPOVER_BORDER}px solid ${COLORS.PRIMARY_1}`,
+  borderRadius: COMMON_SIZE.BORDER_RADIUS,
+  padding: COMMON_SIZE.POPOVER_PADDING,
+});
