e8b4da938a6fdf93e87c3cfe1709ddd8b529d7cd
Min-h-96 <asdf6256@gmail.com>
2022-11-16 12:59:23 +0900

    feat: OAuth 로 얻은 사용자 정보 세션에 저장

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,13 +1,23 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
+
 import { AuthService, UserInfo } from './auth.service';
 
-@Controller('/auth')
+@Controller('/api/auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Get('/google-callback')
-  async GoogleCallback(@Query('code') code: string) {
+  async GoogleCallback(
+    @Query('code') code: string,
+    @Req() req: Request,
+    @Res() res: Response,
+  ) {
     const user: UserInfo = await this.authService.getGoogleInfo(code);
+
+    //세션에 사용자 정보 저장
+    let session: any = req.session;
+    session.user = user;
 
     //DB에서 유저 확인
     //없으면 회원가입으로 redirect
@@ -16,15 +26,22 @@ export class AuthController {
     return user;
   }
 
-  @Get('github-callback')
-  async GithubCallback(@Query('code') code: string) {
+  @Get('/github-callback')
+  async GithubCallback(
+    @Query('code') code: string,
+    @Req() req: Request,
+    @Res() res: Response,
+  ) {
     const user: UserInfo = await this.authService.getGithubInfo(code);
 
-    //세션에 저장
+    //세션에 사용자 정보 저장
+    let session: any = req.session;
+    session.user = user;
+
     //DB에서 유저 확인
     //없으면 회원가입으로 redirect
     //있으면 메인으로 redirect
 
-    return user;
+    return res.status(200).redirect('http://localhost:3001/api/auth/check');
   }
 }
