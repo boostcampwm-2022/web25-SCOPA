919a696826721d1cd001998f8b2417130da127cb
Min-h-96 <asdf6256@gmail.com>
2022-11-16 13:28:57 +0900

    feat: /api/auth/validate API 작성

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -62,4 +62,35 @@ export class AuthController {
       },
     });
   }
+
+  @Get('/validate')
+  validateSignUpInfo(@Query('id') id: string, @Res() res: Response) {
+    // 유효성 검사
+    const regexEngNum = /^[a-zA-Z0-9]*$/;
+    const isValidateLength = id.length >= 4 && id.length <= 15;
+    const isValidateCharacter = regexEngNum.test(id);
+
+    if (!(isValidateLength && isValidateCharacter)) {
+      return res.status(401).send({
+        code: 10001,
+        message: '유효하지 않은 ID 입니다.',
+      });
+    }
+
+    // 중복 확인
+    const isDuplicate = false; // DB 설정이 완료되면 실제 중복을 체크하는 로직을 추가해야 합니다.
+
+    if (isDuplicate) {
+      return res.status(401).send({
+        code: 10002,
+        message: '중복된 ID 입니다.',
+      });
+    }
+
+    // 응답
+    return res.status(200).send({
+      code: 10000,
+      message: '유효한 ID 입니다.',
+    });
+  }
 }
