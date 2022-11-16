a00ab3ed1d4bb3d90dfc75276143f89ec33103b3
chichoon <chichoon.choi@gmail.com>
2022-11-16 15:54:07 +0900

    chore: MiniNavBar 용례 Mainpage에 추가

--- client/src/pages/MainPage/index.tsx
+++ client/src/pages/MainPage/index.tsx
@@ -1,3 +1,16 @@
+import { MiniNavBar } from 'common';
+
 export const MainPage = () => {
-  return <div>아아아</div>;
+  // MiniNavBar 사용례: 저렇게 투명 태그로 감싸 넣어야 space-between 잘 반영 됩니다
+  return (
+    <>
+      <MiniNavBar>
+        <>
+          <span>얼리버드</span>
+          <button type='button'>버튼</button>
+        </>
+      </MiniNavBar>
+      <div>내용내용내용</div>
+    </>
+  );
 };
