de60c424d0518882d5650f321a26e4c937c79a89
chichoon <chichoon.choi@gmail.com>
2022-11-16 15:29:00 +0900

    feat: mini nav bar 추가
    - 메인페이지, 상세페이지에 사용되는 바
    - index.ts에 export 추가

---
+++ client/src/common/MiniNavBar/index.tsx
@@ -0,0 +1,9 @@
+/** @jsxImportSource @emotion/react */
+
+interface Props {
+  children: JSX.Element;
+}
+
+export const MiniNavBar = ({ children }: Props) => {
+  return <div>{children}</div>;
+};
--- client/src/common/index.ts
+++ client/src/common/index.ts
@@ -2,3 +2,5 @@ export { InterestsBox } from './InterestsBox';
 export { TechStackBox } from './TechStackBox';
 export { LoginLayout } from './LoginLayout';
 export { CheckLogin } from './CheckLogin';
+export { CommonLayout } from './CommonLayout';
+export { MiniNavBar } from './MiniNavBar';
