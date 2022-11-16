2cde7525f3b3c3febee1ef6b65d1b872d9df390d
dohyeon-han <limited.hyeon@gmail.com>
2022-11-16 20:30:41 +0900

    chore: 필요없는 파일 삭제

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URL, {
+      dbName: 'test',
+    }),
+  ],
 })
 export class AppModule {}
