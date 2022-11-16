4b5a3a826f50b0305e499bcc787a127a011fb65a
chichoon <chichoon.choi@gmail.com>
2022-11-16 13:34:47 +0900

    refactor: onClick 핸들러만 부착

--- client/src/common/TechStackBox/TechStackCheckbox.tsx
+++ client/src/common/TechStackBox/TechStackCheckbox.tsx
@@ -1,31 +1,19 @@
 /** @jsxImportSource @emotion/react */
 
-import { ChangeEvent, useCallback, useState } from 'react';
+import { ChangeEvent } from 'react';
 
 import { checkboxLabelStyle, checkboxStyle, checkboxWrapperStyle } from './TechStackCheckbox.styles';
 
 interface Props {
-  initialValue: boolean;
+  value: boolean;
   onChange: (e: ChangeEvent<HTMLInputElement>) => void;
   name: string;
 }
 
-export const TechStackCheckbox = ({ initialValue, onChange, name }: Props) => {
-  const [isChecked, setIsChecked] = useState<boolean>(initialValue);
-  const handleChangeCheckbox = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-    setIsChecked(e.target.checked);
-    onChange(e);
-  }, []);
-
+export const TechStackCheckbox = ({ value, onChange, name }: Props) => {
   return (
     <div css={checkboxWrapperStyle}>
-      <input
-        type='checkbox'
-        id={`tech-stack-${name}`}
-        checked={isChecked}
-        onChange={handleChangeCheckbox}
-        css={checkboxStyle}
-      />
+      <input type='checkbox' id={`tech-stack-${name}`} checked={value} onChange={onChange} css={checkboxStyle} />
       <label htmlFor={`tech-stack-${name}`} css={checkboxLabelStyle}>
         {name}
       </label>
