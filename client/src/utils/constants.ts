0c79787e5586bd715839a5a4301b01bf9e5a5af5
chichoon <chichoon.choi@gmail.com>
2022-11-17 01:24:34 +0900

    chore: API 관련 상수들 추가

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -20,3 +20,15 @@ export const STACK_LIST: ReadonlyArray<string> = [
 ];
 
 export const INTEREST_LIST: ReadonlyArray<string> = ['Frontend', 'Backend', 'iOS', 'Android'];
+
+export const API = {
+  LOGOUT: '/auth/logout',
+  CHECK: '/auth/check',
+  VALIDATE: '/auth/validate',
+  REGISTER: '/auth/register',
+  WITHDRAW: '/auth/withdraw',
+  DETAIL: '/detail',
+  EDIT: '/detail/edit',
+  PROFILE: '/profile',
+  LIKE: '/like',
+};
