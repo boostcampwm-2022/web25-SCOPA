2bcad6f3c33fd45dcba99330b0937f9fb92195a7
Min-h-96 <asdf6256@gmail.com>
2022-11-17 13:47:22 +0900

    refactor: auth entity 삭제

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,14 +1,10 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { Auth, authSchema } from './entities/auth.entity';
 
 @Module({
-  imports: [
-    MongooseModule.forFeature([{ name: Auth.name, schema: authSchema }]),
-  ],
+  imports: [],
   controllers: [AuthController],
   providers: [AuthService],
 })
