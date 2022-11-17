d024c08dd8893c82de4a007a0b88cb861e732cb8
chichoon <chichoon.choi@gmail.com>
2022-11-17 14:43:00 +0900

    fix: 유저가 로그인중이 아닐 때에만 fetch하도록 조건문 추가

--- client/src/common/CheckLogin/index.tsx
+++ client/src/common/CheckLogin/index.tsx
@@ -1,28 +1,26 @@
 import { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
-import { useSetRecoilState } from 'recoil';
+import { useRecoilState } from 'recoil';
 
 import { fetchCheckLogin } from 'services';
 import { currentUserState } from 'store';
 
 export const CheckLogin = () => {
-  const setCurrentUser = useSetRecoilState(currentUserState);
+  const [currentUser, setCurrentUser] = useRecoilState(currentUserState);
 
   useEffect(() => {
-    fetchCheckLogin().then((data) => {
-      if (data.code !== 200) setCurrentUser({ id: null });
-      else setCurrentUser({ id: data.body.id });
-    });
-  }, []);
+    if (!currentUser.id) {
+      fetchCheckLogin().then((data) => {
+        if (data.code !== 200) setCurrentUser({ id: null });
+        else setCurrentUser({ id: data.body.id });
+      });
+    }
+  }, []); // 페이지 로딩 시 한 번만 호출
 
   // 전역 상태가 남아있으면, 굳이 fetch 하지 않고 return outlet
   // 전역 상태가 날아갔다면, 새로 받아오기 (쿠키 검증 및 데이터 fetch)
 
-  // 로그인 시에만 접근할 수 있는 페이지일 경우, 매 페이지 이동마다 로그인을 체크하는 컴포넌트
-  // 모든 페이지의 상단에서 동작 (미들웨어처럼)
   // 로그인이 되어 있다면 전역 상태로 로그인 정보 저장하는 등의 로직 수행
-
-  // 전역상태값은 조작가능한가?
 
   return <Outlet />;
 };
