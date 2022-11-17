1b2f4326a5626bb4c25b2bcbc26e0b47e5aafe3c
Min-h-96 <asdf6256@gmail.com>
2022-11-17 13:41:38 +0900

    refactor: user resource 추가 - 인증 관련 로직과 회원 관련 로직을 분리하기 위한 user resource 추가

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { AuthModule } from './auth/auth.module';
       dbName: process.env.DB_NAME,
     }),
     AuthModule,
+    UserModule,
   ],
 })
 export class AppModule {}
