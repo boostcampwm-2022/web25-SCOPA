e83c9c6f5e1ba1e8466ae5a44e8e1650e46a1846
chichoon <chichoon.choi@gmail.com>
2022-11-17 21:10:33 +0900

    feat: 목업 데이터 작성, 간략한 레이아웃 태그들 작성

--- client/src/pages/DetailPage/index.tsx
+++ client/src/pages/DetailPage/index.tsx
@@ -1,3 +1,51 @@
+/** @jsxImportSource @emotion/react */
+
+import { useParams } from 'react-router-dom';
+
+import { MiniNavBar } from 'common';
+
+import { PencilIcon } from 'assets/svgs';
+import { detailProfileWrapperStyle } from './styles';
+
+const MockUpData = {
+  code: `export const DetailPage = () => {
+  const { id } = useParams();
+    return (
+    <div>
+      <MiniNavBar>
+        <>
+          <span>{id}</span>
+          <button type='button'>
+            <PencilIcon />
+          </button>
+        </>
+      </MiniNavBar>
+      상세페이지임 암튼그럼ㅋㅋ
+    </div>
+  );
+};`,
+  skills: ['React', 'Emotion', 'Typescript'],
+  requirements: ['잠실사는사람만', '소통좋아해요'],
+  liked: false,
+};
+
 export const DetailPage = () => {
-  return <div>상세페이지임 암튼그럼ㅋㅋ</div>;
+  const { id } = useParams();
+
+  return (
+    <>
+      <MiniNavBar>
+        <>
+          <span>{id}</span>
+          <button type='button'>
+            <PencilIcon />
+          </button>
+        </>
+      </MiniNavBar>
+      <div css={detailProfileWrapperStyle}>
+        <div>어쩌구</div>
+        <div>저쩌구</div>
+      </div>
+    </>
+  );
 };
