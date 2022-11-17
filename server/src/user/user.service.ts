47d8c7e3d29f47ff5933f5e7094245c87ffd3e1e
Min-h-96 <asdf6256@gmail.com>
2022-11-17 23:39:48 +0900

    refactor: user 관련 파일 import 순서 컨벤션 적용

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { UserInfo } from 'src/d';
 
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
+import { UserInfo } from 'src/d';
 
 @Injectable()
 export class UserService {
