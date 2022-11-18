1342521a2bc39ca910aab6f74e2a31cd4f4165e9
chichoon <chichoon.choi@gmail.com>
2022-11-18 09:51:35 +0900

    feat: 메인페이지에서 상세페이지 이동할 수 있는 링크 추가 (임시)

--- client/src/pages/MainPage/index.tsx
+++ client/src/pages/MainPage/index.tsx
@@ -1,4 +1,5 @@
 import { MiniNavBar } from 'common';
+import { Link } from 'react-router-dom';
 
 export const MainPage = () => {
   // MiniNavBar 사용례: 저렇게 투명 태그로 감싸 넣어야 space-between 잘 반영 됩니다
@@ -10,7 +11,9 @@ export const MainPage = () => {
           <button type='button'>버튼</button>
         </>
       </MiniNavBar>
-      <div>내용내용내용</div>
+      <div>
+        <Link to='/detail/4242'>상세페이지 링크</Link>
+      </div>
     </>
   );
 };
