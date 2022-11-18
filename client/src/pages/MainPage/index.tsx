0421567666cbc8c36253f45bd1d06a1f6a26693c
chichoon <chichoon.choi@gmail.com>
2022-11-18 14:44:11 +0900

    refactor: import 순서 수정

--- client/src/pages/MainPage/index.tsx
+++ client/src/pages/MainPage/index.tsx
@@ -1,5 +1,6 @@
-import { MiniNavBar } from 'common';
 import { Link } from 'react-router-dom';
+
+import { MiniNavBar } from 'common';
 
 export const MainPage = () => {
   // MiniNavBar 사용례: 저렇게 투명 태그로 감싸 넣어야 space-between 잘 반영 됩니다
