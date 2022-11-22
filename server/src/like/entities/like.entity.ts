bd4cebb0064c2cf48e2e473721dffd99d0f2dbc8
Min-h-96 <asdf6256@gmail.com>
2022-11-22 14:16:12 +0900

    feat: Like entity 구현

--- server/src/like/entities/like.entity.ts
+++ server/src/like/entities/like.entity.ts
@@ -1,1 +1,23 @@
-export class Like {}
+import { Document } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { IsArray, IsString } from 'class-validator';
+
+@Schema({
+  versionKey: false,
+  timestamps: { createdAt: 'createdAt', updatedAt: 'updatedAt' },
+})
+export class Like extends Document {
+  @Prop({
+    required: true,
+  })
+  @IsString()
+  userId: string;
+
+  @Prop({
+    required: true,
+  })
+  @IsArray()
+  likedId: string[];
+}
+
+export const likeSchema = SchemaFactory.createForClass(Like);
