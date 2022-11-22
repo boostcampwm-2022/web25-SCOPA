d726cd564eb9d4e5b32eea0538833fe8f39c5ecc
Min-h-96 <asdf6256@gmail.com>
2022-11-22 22:55:01 +0900

    refactor: 공통 로직 함수로 빼기 - 세션에 있는 사용자 정보로 User 조회하고 없다면 에러 반환하는 로직 - id 로 User 조회하고 에러 반환하는 로직

--- server/src/like/like.service.ts
+++ server/src/like/like.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 import { errors } from 'src/common/response/error-response';
 import { UserInfo } from 'src/d';
+import { User } from 'src/user/entities/user.entity';
 import { UserRepository } from 'src/user/user.repository';
 import { AddLikeRequestDto } from './dto/add-like.dto';
 import { DeleteLikeRequestDto } from './dto/delete-like.dto';
@@ -19,22 +20,8 @@ export class LikeService {
 
   // Like Document 에 좋아요 추가
   async addLike(likeDto: AddLikeRequestDto, userInfo: UserInfo) {
-    // 세션에 있는 사용자 정보로 User 의 ObjectId 조회
-    const { authProvider, authId } = { ...userInfo };
-    const user = await this.userRepository.findUserByAuthProviderAndAuthId(
-      authProvider,
-      authId,
-    );
-    // 조회된 user 가 없다면, 에러 반환
-    if (!user) {
-      throw errors.NOT_MATCHED_USER;
-    }
-
-    // 좋아요 리스트에 추가하고자 하는 likedId 가 올바른 지 확인
-    const likedUser = await this.userRepository.findUserById(likeDto.likedId);
-    if (!likedUser) {
-      throw errors.NOT_MATCHED_USER;
-    }
+    const user = await this.checkUserExistByUserInfo(userInfo);
+    const likedUser = await this.checkUserExistById(likeDto.likedId);
 
     // User 의 ObjectId 로 Like Document 조회
     const like = await this.likeRepository.findLikeByUserId(user._id);
@@ -45,22 +32,8 @@ export class LikeService {
   }
 
   async deleteLike(likeDto: DeleteLikeRequestDto, userInfo: UserInfo) {
-    // 세션에 있는 사용자 정보로 User 의 ObjectId 조회
-    const { authProvider, authId } = { ...userInfo };
-    const user = await this.userRepository.findUserByAuthProviderAndAuthId(
-      authProvider,
-      authId,
-    );
-    // 조회된 user 가 없다면, 에러 반환
-    if (!user) {
-      throw errors.NOT_MATCHED_USER;
-    }
-
-    // 좋아요 리스트에 삭제하고자 하는 likedId 가 올바른 지 확인
-    const likedUser = await this.userRepository.findUserById(likeDto.likedId);
-    if (!likedUser) {
-      throw errors.NOT_MATCHED_USER;
-    }
+    const user = await this.checkUserExistByUserInfo(userInfo);
+    const likedUser = await this.checkUserExistById(likeDto.likedId);
 
     // User 의 ObjectId 로 Like Document 조회
     const like = await this.likeRepository.findLikeByUserId(user._id);
@@ -68,5 +41,30 @@ export class LikeService {
 
     // Like Document 에 likedId 필드에 likeDto 의 likedId 값을 추가(update);
     return await this.likeRepository.updateLikeByLikedId(user._id, newLikedId);
+  }
+
+  async checkUserExistByUserInfo(userInfo: UserInfo): Promise<User> {
+    // 세션에 있는 사용자 정보로 User 의 ObjectId 조회
+    const { authProvider, authId } = { ...userInfo };
+    const user = await this.userRepository.findUserByAuthProviderAndAuthId(
+      authProvider,
+      authId,
+    );
+
+    // 조회된 user 가 없다면, 에러 반환
+    if (!user) {
+      throw errors.NOT_MATCHED_USER;
+    }
+
+    return user;
+  }
+
+  async checkUserExistById(id: string): Promise<User> {
+    const user = await this.userRepository.findUserById(id);
+    if (!user) {
+      throw errors.NOT_MATCHED_USER;
+    }
+
+    return user;
   }
 }
