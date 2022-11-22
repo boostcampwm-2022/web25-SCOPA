0c8d20801cf47b13d270c028d48c8661f94401c4
Hseo <sophieseo3@gmail.com>
2022-11-22 21:37:37 +0900

    feat/fe-main-layout: JSX 태그 조건문 단순화

--- client/src/pages/MainPage/InterestFilter.tsx
+++ client/src/pages/MainPage/InterestFilter.tsx
@@ -21,7 +21,7 @@ const InterestFilter = () => {
     <div ref={outSideClickRef} css={interestStyle}>
       <div css={interestFilterWrapperStyle}>
         <button type='button' onClick={handleClick}>
-          {interest.length > 0 ? <span>{interest}</span> : <span>분야 선택</span>}
+          <span>{interest.length > 0 ? interest : '분야 선택'}</span>
         </button>
       </div>
       {isShown && <InterestsBox setInterest={setInterest} setIsShown={setIsShown} />}
