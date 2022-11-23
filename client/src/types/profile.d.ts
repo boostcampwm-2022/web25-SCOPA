0cfe11467bdc97f1d84aef8d1da301814fd59659
chichoon <chichoon.choi@gmail.com>
2022-11-23 23:48:23 +0900

    chore: ProfileType 추가
    - 프로필 Props로 넘겨줄 때 사용
    - 임시로 만든거라 수정될 여지 큼

---
+++ client/src/types/profile.d.ts
@@ -0,0 +1,12 @@
+export interface ProfileType {
+  id: number;
+  code: string;
+  language: string;
+  interest: string;
+  skills: string[];
+  requirements: string[];
+  worktype: string;
+  worktime: string;
+  email: string;
+  liked: boolean;
+}
