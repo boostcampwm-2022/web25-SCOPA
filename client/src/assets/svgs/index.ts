526a768ab1840186b00f079a301c5092772d7a3d
chichoon <chichoon.choi@gmail.com>
2022-11-23 23:47:54 +0900

    chore: 편집 아이콘과 저장 아이콘 추가
    - 아이콘 규격 통일을 위해 기존 아이콘 삭제

--- client/src/assets/svgs/index.ts
+++ client/src/assets/svgs/index.ts
@@ -3,6 +3,8 @@ export { ReactComponent as GithubIcon } from './githubIcon.svg';
 export { ReactComponent as GoogleIcon } from './googleIcon.svg';
 export { ReactComponent as HeartEmptyIcon } from './heartEmptyIcon.svg';
 export { ReactComponent as HeartFilledIcon } from './heartFilledIcon.svg';
-export { ReactComponent as PencilIcon } from './pencilIcon.svg';
 export { ReactComponent as XIcon } from './xIcon.svg';
 export { ReactComponent as SearchIcon } from './searchIcon.svg';
+
+export { ReactComponent as EditIcon } from './editIcon.svg';
+export { ReactComponent as SaveIcon } from './saveIcon.svg';
