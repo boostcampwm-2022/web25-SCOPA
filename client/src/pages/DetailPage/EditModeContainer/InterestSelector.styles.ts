450a9ca26821d1ca095f03496b1d7a43039fe1ec
chichoon <chichoon.choi@gmail.com>
2022-11-24 23:02:38 +0900

    feat: interest selector 별도의 파일로 분리
    - useClickOutside를 interest, techStack 각각 따로 사용해야 하므로

---
+++ client/src/pages/DetailPage/EditModeContainer/InterestSelector.styles.ts
@@ -0,0 +1,34 @@
+import { css } from '@emotion/react';
+
+import { COLORS } from 'styles/colors';
+import { COMMON_SIZE, FONT_SIZE } from 'styles/sizes';
+
+export const interestWrapperStyle = css({
+  display: 'flex',
+  position: 'relative',
+  width: '50%',
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  backgroundColor: COLORS.WHITE,
+  height: COMMON_SIZE.EDITOR_BOX_INPUT_HEIGHT,
+  border: 'none',
+  paddingLeft: 5,
+  borderRadius: COMMON_SIZE.EDITOR_BOX_INPUT_BORDER_RADIUS,
+  marginBottom: COMMON_SIZE.PROFILE_BOX_DD_MARGIN_BOTTOM,
+
+  ' span': {
+    color: COLORS.TEXT_1,
+  },
+});
+
+export const popoverButtonStyle = css({
+  backgroundColor: COLORS.PRIMARY_3,
+  height: COMMON_SIZE.EDITOR_BOX_INPUT_HEIGHT,
+  borderBottomRightRadius: COMMON_SIZE.EDITOR_BOX_INPUT_BORDER_RADIUS,
+  borderTopRightRadius: COMMON_SIZE.EDITOR_BOX_INPUT_BORDER_RADIUS,
+
+  ' svg': {
+    height: '100%',
+  },
+});
