04efbcf0da748d4c62c55936cedf4f9ac4bc07da
chichoon <chichoon.choi@gmail.com>
2022-11-25 00:23:45 +0900

    feat: 기술스택 selector 추가
    - 디자인까지

---
+++ client/src/pages/DetailPage/EditModeContainer/TechStackSelector.styles.ts
@@ -0,0 +1,33 @@
+import { css } from '@emotion/react';
+
+import { COLORS } from 'styles/colors';
+import { COMMON_SIZE, FONT_SIZE } from 'styles/sizes';
+
+export const techStackWrapperStyle = css({
+  display: 'flex',
+  position: 'relative',
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  backgroundColor: COLORS.WHITE,
+  height: COMMON_SIZE.EDITOR_BOX_INPUT_HEIGHT,
+  border: 'none',
+  paddingLeft: 5,
+  borderRadius: COMMON_SIZE.EDITOR_BOX_INPUT_BORDER_RADIUS,
+
+  '> div path:nth-of-type(1)': {
+    fill: 'none',
+  },
+});
+
+export const popoverButtonStyle = css({
+  backgroundColor: COLORS.PRIMARY_3,
+  height: COMMON_SIZE.EDITOR_BOX_INPUT_HEIGHT,
+  borderBottomRightRadius: COMMON_SIZE.EDITOR_BOX_INPUT_BORDER_RADIUS,
+  borderTopRightRadius: COMMON_SIZE.EDITOR_BOX_INPUT_BORDER_RADIUS,
+
+  ' svg': {
+    fill: COLORS.WHITE,
+    height: '100%',
+  },
+});
