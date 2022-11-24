36bf592f544c562b2d68d6840731337c16ba52b7
dohyeon-han <limited.hyeon@gmail.com>
2022-11-25 00:36:41 +0900

    fix: like controller async/await 추가

--- server/src/like/like.controller.ts
+++ server/src/like/like.controller.ts
@@ -21,7 +21,7 @@ export class LikeController {
 
   @Post()
   @HttpCode(HttpStatus.OK)
-  addLike(
+  async addLike(
     @Body() likeDto: AddLikeRequestDto,
     @Session() session: Record<string, any>,
   ) {
@@ -29,14 +29,14 @@ export class LikeController {
       throw errors.NOT_LOGGED_IN;
     }
 
-    this.likeService.addLike(likeDto, session.userId);
+    await this.likeService.addLike(likeDto, session.userId);
 
     return new SuccessResponse();
   }
 
   @Delete()
   @HttpCode(HttpStatus.OK)
-  deleteLike(
+  async deleteLike(
     @Body() likeDto: AddLikeRequestDto,
     @Session() session: Record<string, any>,
   ) {
@@ -44,14 +44,14 @@ export class LikeController {
       throw errors.NOT_LOGGED_IN;
     }
 
-    this.likeService.deleteLike(likeDto, session.user);
+    await this.likeService.deleteLike(likeDto, session.userId);
 
     return new SuccessResponse();
   }
 
   @Get('/:id')
   @HttpCode(HttpStatus.OK)
-  findLikes(@Param('id') id: string) {
+  async findLikes(@Param('id') id: string) {
     // param 에 담긴 id 의 좋아요 리스트를 반환하는 service 로직 호출
 
     // 반한된 좋아요 리스트와 함께 응답
