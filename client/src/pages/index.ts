c7a32c403aafbee057f5f9aae1e0fb887b5f56ab
chichoon <chichoon.choi@gmail.com>
2022-11-25 00:27:40 +0900

    chore: testpage 삭제
    - 필요한 페이지를 다 만들었기 때문에 테스트 페이지는 이제 불필요

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -3,5 +3,3 @@ export { DetailPage } from './DetailPage';
 
 export { LoginPage } from './LoginPage';
 export { RegisterPage } from './RegisterPage';
-
-export { TestPage } from './TestPage'; // 테스트용 페이지: 나중에 지워도 됨
