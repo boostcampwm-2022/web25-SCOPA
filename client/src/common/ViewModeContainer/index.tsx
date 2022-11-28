748dbab05baf39ae9a07ccab19a895eed10cc8ae
chichoon <chichoon.choi@gmail.com>
2022-11-29 03:14:12 +0900

    feat: 관심분야 뱃지 추가

--- client/src/common/ViewModeContainer/index.tsx
+++ client/src/common/ViewModeContainer/index.tsx
@@ -6,9 +6,16 @@ import { BottomProfileBox } from './BottomProfileBox';
 import { CodeBox } from './CodeBox';
 import { TopProfileBox } from './TopProfileBox';
 
-import { nicknameStyle, editButtonStyle, detailProfileWrapperStyle } from './styles';
+import {
+  nicknameStyle,
+  editButtonStyle,
+  detailProfileWrapperStyle,
+  nicknameSectionStyle,
+  interestStyle,
+} from './styles';
 
 import { EditIcon } from 'assets/svgs';
+import { INTEREST_COLOR_BASE, INTEREST_COLOR_BORDER, INTEREST_KOR } from './constants';
 
 interface Props {
   profileData: ProfileType;
@@ -17,11 +24,18 @@ interface Props {
 }
 
 export const ViewModeContainer = ({ profileData, onClickEditButton, isMine }: Props) => {
+  const { interest } = profileData;
+
   return (
     <>
       <MiniNavBar>
         <>
-          <h2 css={nicknameStyle}>{profileData.nickname}</h2>
+          <div css={nicknameSectionStyle}>
+            <h2 css={nicknameStyle}>{profileData.nickname}</h2>
+            <div css={interestStyle(INTEREST_COLOR_BASE[interest], INTEREST_COLOR_BORDER[interest])}>
+              <span>{INTEREST_KOR[interest]}</span>
+            </div>
+          </div>
           {isMine && (
             <Button css={editButtonStyle} onClick={onClickEditButton}>
               <>
