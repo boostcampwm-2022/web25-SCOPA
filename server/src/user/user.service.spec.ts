859b82273f00c55befe9715ca593f72e902b8051
dohyeon-han <limited.hyeon@gmail.com>
2022-11-28 22:00:34 +0900

    test: userService test 작성

--- server/src/user/user.service.spec.ts
+++ server/src/user/user.service.spec.ts
@@ -56,7 +56,7 @@ describe('UserService', () => {
     userService = module.get<UserService>(UserService);
   });
 
-  it('유저 전체 조회', () => {
+  it('유저 전체를 조회한다.', () => {
     const users = [userStub];
 
     mockUserRepository.findAll.mockResolvedValue(users); // 비동기로 전체 유저 조회
@@ -66,7 +66,7 @@ describe('UserService', () => {
     });
   });
 
-  it('유저 생성', async () => {
+  it('유저를 생성한다.', async () => {
     const userDto = plainToInstance(CreateUserRequestDto, {
       username: userStub.username,
       interest: userStub.interest,
@@ -93,7 +93,7 @@ describe('UserService', () => {
     expect(createdUser).toEqual(userStub);
   });
 
-  it('생성 유저의 username이 4미만이면 오류', () => {
+  it('생성 유저의 username이 4미만이면 오류가 발생한다.', () => {
     const userDto = plainToInstance(CreateUserRequestDto, {
       username: 'abc',
       interest: userStub.interest,
@@ -110,7 +110,7 @@ describe('UserService', () => {
     );
   });
 
-  it('생성 유저의 username이 15초과이면 오류', () => {
+  it('생성 유저의 username이 15초과이면 오류가 발생한다.', () => {
     const userDto = plainToInstance(CreateUserRequestDto, {
       username: 'abcdefghijklmnop',
       interest: userStub.interest,
@@ -127,7 +127,7 @@ describe('UserService', () => {
     );
   });
 
-  it('알파벳이나 숫자로 구성되지 않은 username은 오류', () => {
+  it('알파벳이나 숫자로 구성되지 않은 username은 오류가 발생한다.', () => {
     const userDto1 = plainToInstance(CreateUserRequestDto, {
       username: 'abcd!',
       interest: userStub.interest,
@@ -160,7 +160,7 @@ describe('UserService', () => {
     );
   });
 
-  it('중복된 username은 오류', () => {
+  it('중복된 username은 오류가 발생한다.', () => {
     const userDto = plainToInstance(CreateUserRequestDto, {
       username: userStub.username,
       interest: userStub.interest,
@@ -179,5 +179,39 @@ describe('UserService', () => {
     expect(userService.createUser(userDto, authInfo)).rejects.toEqual(
       errors.ID_DUPLICATED,
     );
+  });
+
+  it('authProvider와 authId로 유저를 조회한다.', async () => {
+    const authProvider = 'github';
+    const authId = '12345';
+    when(mockUserRepository.findUserByAuthProviderAndAuthId)
+      .calledWith(authProvider, authId)
+      .mockResolvedValue(userStub);
+
+    const user = await userService.findOne(authProvider, authId);
+    expect(user).toEqual(userStub);
+  });
+
+  it('userId로 유저를 삭제한다.', async () => {
+    const userId = '12345678';
+    const deleteResult = { acknowledged: true, deletedCount: 1 };
+    when(mockUserRepository.findUserById)
+      .calledWith(userId)
+      .defaultResolvedValue(userStub);
+
+    when(mockUserRepository.deleteById)
+      .calledWith(userId)
+      .defaultResolvedValue(deleteResult);
+
+    expect(await userService.remove(userId)).toEqual(deleteResult);
+  });
+
+  it('삭제하려는 userId가 없으면 오류가 발생한다.', () => {
+    const userId = '12345678';
+    when(mockUserRepository.findUserById)
+      .calledWith(userId)
+      .defaultResolvedValue(null);
+
+    expect(userService.remove(userId)).rejects.toEqual(errors.NOT_MATCHED_USER);
   });
 });
