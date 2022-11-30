a08241a04c0269c6564a443e7fd6b7d573a8fbf1
Min-h-96 <asdf6256@gmail.com>
2022-11-30 22:47:17 +0900

    test: Supertest 세션 사용법 확인

--- server/src/like/like.integration.spec.ts
+++ server/src/like/like.integration.spec.ts
@@ -2,7 +2,7 @@ import * as request from 'supertest';
 import { Test } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import { MongoMemoryServer } from 'mongodb-memory-server';
-import { connect, Connection, Model } from 'mongoose';
+import { connect, Connection, Model, Types } from 'mongoose';
 import { getModelToken } from '@nestjs/mongoose';
 
 import { User, userSchema } from 'src/user/entities/user.entity';
@@ -33,6 +33,12 @@ describe('Like 모듈 통합 테스트', () => {
       .compile();
 
     app = module.createNestApplication();
+    app.use((req, res, next) => {
+      req.session = {
+        userId: new Types.ObjectId().toString(),
+      };
+      next();
+    });
     await app.init();
   });
 
@@ -51,7 +57,18 @@ describe('Like 모듈 통합 테스트', () => {
     }
   });
 
-  it('GET /api/like/:id', () => {
-    return request(app.getHttpServer()).get('/api/like/1').expect(200);
+  it('GET /api/like/:id', async () => {
+    const res = await request(app.getHttpServer())
+      .get('/api/like/1')
+      .expect(200);
+
+    console.log(res.body);
+  });
+
+  it('test', async () => {
+    const res = await request(app.getHttpServer()).post('/api/like');
+    // .send({ likedId: new Types.ObjectId().toString() });
+
+    console.log(res.body);
   });
 });
