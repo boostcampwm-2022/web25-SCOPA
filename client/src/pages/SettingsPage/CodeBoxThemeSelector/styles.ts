cc2673e7d791c5473490295ba75b24b1372243c0
chichoon <chichoon.choi@gmail.com>
2022-11-30 19:17:08 +0900

    design: 코드박스 선택 컨테이너 스타일링

---
+++ client/src/pages/SettingsPage/CodeBoxThemeSelector/styles.ts
@@ -0,0 +1,30 @@
+import { css } from '@emotion/react';
+
+import { COLORS } from 'styles/colors';
+import { COMMON_SIZE, FONT_SIZE } from 'styles/sizes';
+
+export const codeBoxListStyle = css({
+  display: 'flex',
+  flexDirection: 'row',
+});
+
+export const codeListElementStyle = (isSelected: boolean) =>
+  css({
+    textAlign: 'start',
+    padding: 0,
+    height: 'fit-content',
+    borderRadius: COMMON_SIZE.BORDER_RADIUS,
+    marginRight: 10,
+    border: isSelected ? `3px solid ${COLORS.PRIMARY_1}` : `1px solid ${COLORS.BOX_BORDER}`,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+
+    '> span': {
+      marginTop: 10,
+      marginBottom: 5,
+      fontSize: FONT_SIZE.MEDIUM,
+      fontStyle: 'italic',
+      color: COLORS.TEXT_2,
+    },
+  });
