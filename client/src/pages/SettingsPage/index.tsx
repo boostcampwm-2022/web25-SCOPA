438e98ba621d64cb6de4ab2c405b32aa6f954cee
chichoon <chichoon.choi@gmail.com>
2022-11-30 15:55:24 +0900

    feat: 환경설정 페이지 레이아웃 1차 구성
    - 툴팁 추가 (물음표 아이콘에 호버링하면 등장)
    - 세 개의 섹션 추가
    - 디자인

--- client/src/pages/SettingsPage/index.tsx
+++ client/src/pages/SettingsPage/index.tsx
@@ -1,12 +1,60 @@
+/** @jsxImportSource @emotion/react */
+
+import { useCallback, useState } from 'react';
+
 import { MiniNavBar, NavSubtitle } from 'common';
+import { Tooltip } from './Tooltip';
+
+import {
+  settingsWrapperStyle,
+  settingsBackgroundStyle,
+  settingsListStyle,
+  tooltipIconStyle,
+  subtitleWrapperStyle,
+} from './styles';
+
+import { QuestionIcon } from 'assets/svgs';
 
 export const SettingsPage = () => {
+  const [isTooltipShown, setIsTooltipShown] = useState(false);
+  const handleMouseOverTooltip = useCallback(() => {
+    setIsTooltipShown(true);
+  }, []);
+
+  const handleMouseOutTooltip = useCallback(() => {
+    setIsTooltipShown(false);
+  }, []);
+
   return (
     <>
       <MiniNavBar>
-        <NavSubtitle text='환경설정' />
+        <div css={subtitleWrapperStyle}>
+          <NavSubtitle text='환경설정' />
+          <div
+            css={tooltipIconStyle}
+            onFocus={handleMouseOverTooltip}
+            onMouseOver={handleMouseOverTooltip}
+            onBlur={handleMouseOutTooltip}
+            onMouseOut={handleMouseOutTooltip}
+          >
+            <QuestionIcon />
+          </div>
+          {isTooltipShown && <Tooltip text='설정값은 로컬 스토리지에 저장됩니다.' />}
+        </div>
       </MiniNavBar>
-      <div>환경설정임</div>
+      <div css={settingsBackgroundStyle}>
+        <ul css={settingsWrapperStyle}>
+          <li css={settingsListStyle}>
+            <h3>코드 뷰어 테마</h3>
+          </li>
+          <li css={settingsListStyle}>
+            <h3>코드 글자 크기</h3>
+          </li>
+          <li css={settingsListStyle}>
+            <h3>화면 밝기</h3>
+          </li>
+        </ul>
+      </div>
     </>
   );
 };
---
+++ client/src/pages/SettingsPage/styles.ts
@@ -0,0 +1,61 @@
+import { css } from '@emotion/react';
+
+import { COLORS } from 'styles/colors';
+import { COMMON_SIZE, FONT_SIZE } from 'styles/sizes';
+
+export const settingsBackgroundStyle = css({
+  display: 'flex',
+  height: '69vh',
+  paddingLeft: 30,
+  paddingRight: 30,
+});
+
+export const settingsWrapperStyle = css({
+  width: '100%',
+  height: '100%',
+  borderRadius: COMMON_SIZE.BORDER_RADIUS,
+  border: `${COMMON_SIZE.LINE_WIDTH}px solid ${COLORS.BOX_BORDER}`,
+  backgroundColor: COLORS.WHITE,
+  overflow: 'scroll',
+});
+
+export const settingsListStyle = css({
+  padding: 20,
+  borderBottom: `${COMMON_SIZE.LINE_WIDTH}px solid ${COLORS.BOX_BORDER}`,
+
+  ' h3': {
+    fontSize: FONT_SIZE.LARGE,
+    color: COLORS.TEXT_1,
+    marginBottom: 20,
+  },
+
+  ':last-of-type': {
+    border: 'none',
+  },
+});
+
+export const subtitleWrapperStyle = css({
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  position: 'relative',
+});
+
+export const tooltipIconStyle = css({
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'center',
+  position: 'relative',
+  width: 20,
+  height: 20,
+  borderRadius: 10,
+  marginLeft: 10,
+  border: `1px solid ${COLORS.BOX_BORDER}`,
+
+  ' svg': {
+    width: 15,
+    height: 15,
+    fill: COLORS.BOX_BORDER,
+  },
+});
