c09b89d90c06ef8c3661e0cc64284f8836f26d5c
Min-h-96 <asdf6256@gmail.com>
2022-11-30 20:37:17 +0900

    test: Like 모듈 통합 테스트 기초 설정

---
+++ server/src/like/like.integration.spec.ts
@@ -0,0 +1,57 @@
+import * as request from 'supertest';
+import { Test } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import { connect, Connection, Model } from 'mongoose';
+import { getModelToken } from '@nestjs/mongoose';
+
+import { User, userSchema } from 'src/user/entities/user.entity';
+import { Like, likeSchema } from './entities/like.entity';
+import { LikeModule } from './like.module';
+
+describe('Like 모듈 통합 테스트', () => {
+  let app: INestApplication;
+  let mongod: MongoMemoryServer;
+  let mongoConnection: Connection;
+  let userModel: Model<User>;
+  let likeModel: Model<Like>;
+
+  beforeAll(async () => {
+    mongod = await MongoMemoryServer.create();
+    const uri = mongod.getUri();
+    mongoConnection = (await connect(uri)).connection;
+    userModel = mongoConnection.model(User.name, userSchema);
+    likeModel = mongoConnection.model(Like.name, likeSchema);
+
+    const module = await Test.createTestingModule({
+      imports: [LikeModule],
+    })
+      .overrideProvider(getModelToken(User.name))
+      .useValue(userModel)
+      .overrideProvider(getModelToken(Like.name))
+      .useValue(likeModel)
+      .compile();
+
+    app = module.createNestApplication();
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await mongoConnection.dropDatabase();
+    await mongoConnection.close();
+    await mongod.stop();
+    await app.close();
+  });
+
+  afterEach(async () => {
+    const collections = mongoConnection.collections;
+    for (const key in collections) {
+      const collection = collections[key];
+      await collection.deleteMany({});
+    }
+  });
+
+  it('GET /api/like/:id', () => {
+    return request(app.getHttpServer()).get('/api/like/1').expect(200);
+  });
+});
