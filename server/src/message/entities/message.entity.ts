6cac2833866b7a65c56bb1bd4ca97f4d008c1d6a
Min-h-96 <asdf6256@gmail.com>
2022-12-07 14:12:13 +0900

    feat: message 엔티티 & content 엔티티 추가

--- server/src/message/entities/message.entity.ts
+++ server/src/message/entities/message.entity.ts
@@ -1,1 +1,18 @@
-export class Message {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { IsString } from 'class-validator';
+import { HydratedDocument } from 'mongoose';
+import { Content } from './content.entity';
+
+export type MessageDocument = HydratedDocument<Message>;
+
+@Schema({ versionKey: false })
+export class Message {
+  @Prop({ required: true })
+  @IsString()
+  participants: string;
+
+  @Prop({ required: true })
+  contents: Content[];
+}
+
+export const messageSchema = SchemaFactory.createForClass(Message);
