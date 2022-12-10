ebed794cc4412d7fab375f35eb482cd16739ef60
chichoon <chichoon.choi@gmail.com>
2022-12-10 19:55:30 +0900

    design: 미디어 쿼리 상수 추가
    - 핸드폰, 태블릿, PC 기준만 일단 추가

---
+++ client/src/styles/mediaQuery.ts
@@ -0,0 +1,10 @@
+export const MEDIA_QUERY = {
+  PHONE: `@media only screen and (max-width: 575px)`,
+  TABLET: `@media only screen and (min-width: 576px) and (max-width: 767px)`,
+  PC: `@media only screen and (min-width: 768px)`,
+};
+
+// https://getbootstrap.com/docs/5.0/layout/breakpoints/
+// 핸드폰 기준: 통상적으로 576px까지
+// 태블릿 기준: 통상적으로 768px까지
+// 그 이상은 보통 PC 및 모니터로 취급
