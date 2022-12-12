349bb3b12e1dbc3832bab10de0038081af0b80a4
Hseo <sophieseo3@gmail.com>
2022-12-12 20:34:57 +0900

    design: 브라우저 사이즈에 따라, 프로필 개수 조정

--- client/src/pages/MainPage/ProfileList.tsx
+++ client/src/pages/MainPage/ProfileList.tsx
@@ -1,50 +1,20 @@
 /** @jsxImportSource @emotion/react */
 
-import { useCallback, useEffect, useRef, useState } from 'react';
-
 import Profile from './Profile';
 import { singleProfileData } from './types';
 
-import { emptyProfileBoxStyle, profileListStyle } from './styles';
-import { COMMON_SIZE } from 'styles/sizes';
+import { profileListStyle } from './styles';
 
 interface Props {
   profileData: Array<singleProfileData>;
 }
 
 export const ProfileList = ({ profileData }: Props) => {
-  const profileListRef = useRef<HTMLDivElement>(null);
-  const [isOdd, setIsOdd] = useState<boolean>(false);
-  const [isBlankNeeded, setIsBlankNeeded] = useState<boolean>(false);
-
-  useEffect(() => {
-    if (profileData.length % 2 !== 0) setIsOdd(true);
-    else setIsOdd(false);
-  }, []);
-
-  const isWidthDouble = useCallback((targetWidth: number) => {
-    return COMMON_SIZE.PROFILELIST_SINGLE_WIDTH < targetWidth && targetWidth < COMMON_SIZE.PROFILELIST_TRIPLE_WIDTH;
-  }, []);
-
-  const decideBlank = useCallback(() => {
-    if (!profileListRef.current) return;
-    if (isWidthDouble(profileListRef.current.clientWidth)) setIsBlankNeeded(true);
-    else setIsBlankNeeded(false);
-  }, [profileListRef.current]);
-
-  useEffect(() => {
-    window.addEventListener('resize', decideBlank);
-    return () => {
-      window.removeEventListener('resize', decideBlank);
-    };
-  }, []);
-
   return (
-    <div css={profileListStyle} ref={profileListRef}>
+    <div css={profileListStyle}>
       {profileData.map((data) => (
         <Profile key={`profile-${data.id}`} singleData={data} />
       ))}
-      {isOdd && isBlankNeeded && <div css={emptyProfileBoxStyle} />}
     </div>
   );
 };
